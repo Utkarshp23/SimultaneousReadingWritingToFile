a1bd2b707ef1c4f58fdeac141980e2fe8cffbcac
Utkarshp23 <utkarshpawar2332@gmail.com>
2022-10-22 15:52:02 +0530

    Added main method

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,27 @@
 package app;
 
+import java.io.File;
+
+import threads.FileReader1;
+import threads.FileWriter1;
+import resource.Postman;
+
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		File f1 = new File("/home/utkarsh/eclipse-workspace/RawData/demo1.txt");
+		Postman p = new Postman();
 
+		FileWriter1 w = new FileWriter1(f1, p);
+		FileReader1 r = new FileReader1(f1, p);
+		System.out.println("File Copying Started......");
+		w.getT().start();
+
+		r.getT().start();
+
+		r.getT().join();
+		w.getT().join();
+		System.out.println("File Copying Ended......");
 	}
 
 }
